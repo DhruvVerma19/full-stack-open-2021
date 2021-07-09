0640a07e0e3c06043eeafbf607984e7ba2fac583
DhruvVerma19 <dhruv.verma@edu.turkuamk.fi>
2021-07-09 21:01:10 +0300

    Completed till exercises 2.9

--- part2/The Phonebook/src/App.js
+++ part2/The Phonebook/src/App.js
@@ -8,27 +8,50 @@ const App = () => {
       name: "Arto Hellas",
       number: "000-000000"
   
-    }
+    },
+    
+    { name: 'Ada Lovelace',
+      number: '39-44-5323523' 
+    },
+    
+    { name: 'Dan Abramov', 
+      number: '12-43-234345' 
+    },
+    
+    { name: 'Mary Poppendieck', 
+      number: '39-23-6423122' 
+  }
   ];
+
   const [persons, setPersons] = useState(beginingPersonList);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [nameFilter, setNameFilter] = useState("");
   
   const personName = persons.map(person => {
     return person.name.toLocaleUpperCase();
   });
 
-  const personsList = persons.map(person => {
-    return <Person key={person.name} name={person.name} number={person.number}/>;
+    const filterPersonList = persons.filter(person => {
+    return person.name.toUpperCase().includes(nameFilter.toUpperCase());
   });
 
+  const personsList = filterPersonList.map(person => {
+    return <Person key={person.name} name={person.name} number={person.number}/>;
+  });
+  
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
   
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
+
+  const handleFilterChange = (event) => {
+    setNameFilter(event.target.value);
+  };
+
   const formSubmit = (event) => {
     event.preventDefault();
 
@@ -50,6 +73,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with:{" "}
+      <input onChange={(event) => handleFilterChange(event)} value={nameFilter}/>
+      <h2>add a new</h2>
       <form onSubmit={(event) => formSubmit(event)}>
         <div>
           name:{" "}
