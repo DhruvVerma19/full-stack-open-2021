27474bbb1e88a90f4406befffec9ac5f7d5d5142
DhruvVerma19 <dhruv.verma@edu.turkuamk.fi>
2021-07-18 20:09:52 +0300

    Applied morgan to the application

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 
 let persons = [
@@ -26,6 +27,15 @@ let persons = [
 const app = express();
 app.use(express.json())
 
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+  ].join(' ')
+}))
 
 app.get('/api/persons', (request, response) => {
  response.json(persons);
