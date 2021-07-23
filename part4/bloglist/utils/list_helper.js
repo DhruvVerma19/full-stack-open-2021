7f35e74bc79bbc7ea5276e1764df9ccfc70b9f11
DhruvVerma19 <dhruv.verma@edu.turkuamk.fi>
2021-07-23 15:22:23 +0300

    Exercises 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,6 +3,20 @@ const dummy = (blogs) => {
   return Number(blogs + 1)
 }
 
+const totalLikes = (blogs) => {
+  if(!Array.isArray(blogs) || !blogs.length){
+    return 0
+  }
+
+  else if(blogs.length === 1)
+  { return blogs[0].likes
+  }
+  else{
+    return blogs.reduce((total, blog) => total + blog.likes, 0)
+  }
+}
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
