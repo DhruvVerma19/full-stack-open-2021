cc728196c1e2302341ce8b5ac4deb2626d497411
DhruvVerma19 <dhruv.verma@edu.turkuamk.fi>
2021-07-24 02:29:58 +0300

    Exercises till 4.12

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,20 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', async (request, response) => {
+blogsRouter.get('/', async(request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.get('/:id', async(request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/blog.test.js
+++ part4/bloglist/tests/blog.test.js
@@ -22,6 +22,67 @@ test('blogs are returned as json', async() => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
+test('query if a blog has id or _id', async() => {
+  const single_blog = await helper.blogsInDb()
+
+  expect(single_blog[0].id).toBeDefined()
+  expect(single_blog[0]._id).toBe(undefined)
+})
+
+test('a valid blog can be added', async() => {
+  const new_blog = {
+    title: 'Test an app',
+    author: 'Jhon Doe',
+    url: 'https://fullstackopen.com/',
+    likes: 4
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(new_blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const end_blogs = await helper.blogsInDb()
+  expect(end_blogs).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = end_blogs.map(t => t.title)
+  expect(titles).toContain('Test an app')
+})
+
+test('verify if likes property is missing then it defaults to 0', async () => {
+  const new_blog = {
+    title: 'Test an app',
+    author: 'Jhon Doe',
+    url: 'https://fullstackopen.com/'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(new_blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.likes).toBeDefined()
+  expect(response.body.likes).toBe(0)
+})
+
+test('blog without url is not added', async () => {
+  const new_blog = {
+    title: 'Test an app',
+    author: 'Jhon Doe'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(new_blog)
+    .expect(400)
+
+  const end_blogs = await helper.blogsInDb()
+
+  expect(end_blogs).toHaveLength(helper.initialBlogs.length)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
